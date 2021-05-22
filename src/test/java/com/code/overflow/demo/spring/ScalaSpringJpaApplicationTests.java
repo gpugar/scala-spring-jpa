86f561d1680b306bf4db8288167e5d3a34cb911b
Goran Pugar <goranpg@gmail.com>
2021-05-22 13:00:30 +0200

    Add initial spring setup

---
+++ src/test/java/com/code/overflow/demo/spring/ScalaSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.overflow.demo.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalaSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
