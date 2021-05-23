1078fda756da4f515bbbd5911dc5947c09d48aaa
Goran Pugar <goranpg@gmail.com>
2021-05-23 13:01:38 +0200

    Remove obsolete temporal data and jdbcTemplate

--- src/test/scala/com/code/overflow/demo/spring/BaseScalaJpaTest.scala
+++ src/test/scala/com/code/overflow/demo/spring/BaseScalaJpaTest.scala
@@ -7,16 +7,11 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.{DataJpaTest, TestEntityManager}
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.TestContextManager
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.support.TransactionTemplate
-
-import java.time.format.DateTimeFormatter
-import java.time.temporal.TemporalAccessor
-import java.time.{LocalDateTime, ZoneId, ZonedDateTime}
 
 @RunWith(classOf[SpringRunner])
 @DataJpaTest
@@ -36,9 +31,6 @@ abstract class BaseScalaJpaTest
   @Autowired
   var entityManager: TestEntityManager = _
 
-  @Autowired
-  var jdbcTemplate: JdbcTemplate = _
-
   val manager = new TestContextManager(this.getClass)
 
   override protected def beforeAll(): Unit =
@@ -52,15 +44,6 @@ abstract class BaseScalaJpaTest
 
   def persist[E](e: E): E =
     transactionTemplate.execute(_ => entityManager.persist(e))
-
-  val temporalMap = Map(
-    (1, createLocalDateTime("2021-01-01 01:00")),
-    (2, createLocalDateTime("2021-01-01 02:00")),
-    (3, createLocalDateTime("2021-01-01 03:00")),
-    (4, createLocalDateTime("2021-01-01 04:00")),
-    (5, createLocalDateTime("2021-01-01 05:00")),
-    (6, createLocalDateTime("2021-01-01 06:00"))
-  )
 
   private def initTest(): Unit = {
     val meters = (1 to 10).toList
@@ -78,7 +61,4 @@ abstract class BaseScalaJpaTest
       }).map(persist)
     })
   }
-
-  private def createLocalDateTime(date: String): TemporalAccessor =
-    ZonedDateTime.of(LocalDateTime.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")), ZoneId.systemDefault)
 }
